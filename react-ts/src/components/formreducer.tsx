13a25d26cf55abbc1f109eabec3a596ae8840dbb
Soitahi <paymore@PayMores-MacBook-Pro.local>
2025-03-25 10:57:16 +0300

    changes to the form UI

--- react-ts/src/components/formreducer.tsx
+++ react-ts/src/components/formreducer.tsx
@@ -1,7 +1,46 @@
+import { useReducer } from "react"
+
+const initialState = {
+    name:"", email:"", password:""
+}
+
+const formfillReducer = (state, action) =>{
+    switch (action.type){
+        case "UPDATE_FIELD":
+            return {...state, [action.field]:action.value};
+            case "RESET":
+                return initialState;
+                default:
+                    return state;
+    }
+}
+
+
 
 const Formreducer = () => {
+    const [state, dispatch] = useReducer(formfillReducer, initialState);
+    const handleChange = (e) =>{
+        dispatch({type:"UPDATE_FIELD", field:e.target.name, value:e.target.value})
+        }
+        const handleSubmit = (e) =>{e.preventDefault();
+            console.log("Form Submitted:", state);
+        };
   return (
-    <div>Formreducer</div>
+    <div>
+        <form onSubmit={handleSubmit} className='form'>
+            <h1>Fill your details</h1>
+            <input type="text" placeholder="Enter your name" name="name" value={state.name} onChange={handleChange} />
+            <input type="email" placeholder="Enter your Email" name="email" value={state.email} onChange={handleChange} />
+            <input type="password" placeholder="Password" name="password" value={state.password} onChange={handleChange} />
+            <div className="buttonCard">
+            <button type="submit">Submit</button>
+            <button type="button" onClick={() => dispatch({ type: "RESET" })}>
+          Reset
+        </button>
+            </div>
+            
+        </form>
+    </div>
   )
 }
 
